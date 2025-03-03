f127d09bd47a9cef96767a5656879be36c7d2ab9
mehedi hasan <mhasanwk@gmail.com>
2025-03-03 15:23:44 -0500

    i am uploading my project for the first time

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module MYFIRSTPROJ {
+}
---
+++ src/practice/helloworld.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("This is my first project on Java");
+		System.out.println("by Mehdi Hasan");
+		System.out.println(20+50);
+		System.out.println("hello class");
+		System.out.println(50-20);
+	}
+
+}
